3cde75f1480403fe1e25f714f88a8ef7751e3d91
lucasgaraglia <garaglialucas@gmail.com>
2025-06-22 10:57:04 -0300

    custom ports support

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ class URL:
 
         if "/" not in url:
             url += "/"
+
         self.host, url = url.split("/", 1)
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
         self.path = "/" + url
 
     def request(self):
